c91e05be52f2c100ce1cbadeacae4b86f38559fc
gmriggs <gmriggs@gmail.com>
2019-10-31 06:21:19 -0400

    getting rid of gotos in MostlyConsecutiveInts,refactoring into Pack and Unpack methods

--- Source/ACE.Entity/MostlyConsecutiveIntSet.cs
+++ Source/ACE.Entity/MostlyConsecutiveIntSet.cs
@@ -36,96 +36,81 @@ namespace ACE.Entity
 
         public void Serialize(Archive archive)
         {
-            var isPacked = archive.Flags.HasFlag(ArchiveFlag.IsPacked);
-            if (isPacked)
-            {
-                Sort();
-                archive.CheckAlignment(4);
-                var bytes = archive.GetBytes(4);
-                var size = Ints.Count;
-                if (bytes != null)
-                {
-                    if (isPacked)
-                        bytes = BitConverter.GetBytes(size);
-                    else
-                        size = BitConverter.ToInt32(bytes, 0);
-                }
-                if (size == 0)
-                    return;
+            if (archive.Flags.HasFlag(ArchiveFlag.IsPacked))
+                SerializePacked(archive);
+            else
+                SerializeUnpacked(archive);
+        }
 
-                int i = 0;
-                int j = 0;
-
-                while (true)
-                {
-                    j = i;
-                    if (i < size)
-                    {
-                        // navigate to the first gap, if any..
-                        var curInt = Ints[i];
-                        var consecutive = curInt;
-                        do
-                        {
-                            if (consecutive != curInt)
-                                break;
-                            ++j;
-                            ++consecutive;
-                            curInt = Ints[j];
-                        }
-                        while (j < size);
-                    }
-
-                    var negFirstConsecutiveSpan = i - j;
-
-                    if (negFirstConsecutiveSpan >= -2)
-                    {
-                        var prevInt = Ints[i++];
-                        var masked = prevInt & 0x7FFFFFFF;
-
-                        archive.CheckAlignment(4);
-
-                        var nextBytes = archive.GetBytes(4);
-                        if (nextBytes != null && isPacked)
-                            nextBytes = BitConverter.GetBytes(masked);
-                    }
-                    else
-                    {
-                        archive.CheckAlignment(4);
-
-                        var nextBytes = archive.GetBytes(4);
-                        if (nextBytes != null)
-                        {
-                            if (isPacked)
-                                nextBytes = BitConverter.GetBytes(negFirstConsecutiveSpan);
-                            else
-                                negFirstConsecutiveSpan = BitConverter.ToInt32(nextBytes, 0);
-                        }
-
-                        var aCurInt = Ints[i];
-                        archive.CheckAlignment(4);
-                        var lastBytes = archive.GetBytes(4);
-                        if (lastBytes != null)
-                        {
-                            if (isPacked)
-                            {
-                                lastBytes = BitConverter.GetBytes(aCurInt);
-                                i -= negFirstConsecutiveSpan;
-                                // size
-                                // goto LABEL_26
-                            }
-                            aCurInt = BitConverter.ToInt32(lastBytes, 0);
-                        }
-                        // size
-                        i -= negFirstConsecutiveSpan;
-                    }
-                    // LABEL_26:
-                    if (i >= size)
-                        return;
-                }
-            }
-            if (isPacked)
+        public void SerializePacked(Archive archive)
+        {
+            Sort();
+            archive.CheckAlignment(4);
+            var bytes = archive.GetBytes(4);
+            var size = Ints.Count;
+            if (bytes != null)
+                bytes = BitConverter.GetBytes(size);
+            if (size == 0)
                 return;
 
+            int i = 0;
+            int j = 0;
+
+            while (i < size)
+            {
+                j = i;
+                // navigate to the first gap, if any..
+                var curInt = Ints[i];
+                var consecutive = curInt;
+                do
+                {
+                    if (consecutive != curInt)
+                        break;
+                    ++j;
+                    ++consecutive;
+                    curInt = Ints[j];
+                }
+                while (j < size);
+
+                var negConsecutiveSpan = i - j;
+
+                if (negConsecutiveSpan >= -2)
+                {
+                    var prevInt = Ints[i++];
+                    var masked = prevInt & 0x7FFFFFFF;
+
+                    archive.CheckAlignment(4);
+
+                    var nextBytes = archive.GetBytes(4);
+                    if (nextBytes != null)
+                        nextBytes = BitConverter.GetBytes(masked);
+                }
+                else
+                {
+                    archive.CheckAlignment(4);
+
+                    var nextBytes = archive.GetBytes(4);
+                    if (nextBytes != null)
+                        nextBytes = BitConverter.GetBytes(negConsecutiveSpan);
+
+                    var aCurInt = Ints[i];
+                    archive.CheckAlignment(4);
+                    var lastBytes = archive.GetBytes(4);
+                    if (lastBytes != null)
+                    {
+                        lastBytes = BitConverter.GetBytes(aCurInt);
+                        i -= negConsecutiveSpan;
+                        // size
+                        continue;
+                    }
+                    // size
+                    i -= negConsecutiveSpan;
+                }
+            }
+        }
+
+        public void SerializeUnpacked(Archive archive)
+        {
             var k = 0;
             var l = 0;
             var finalBytesVal = 0;
@@ -134,25 +119,20 @@ namespace ACE.Entity
             var finalBytes = archive.GetBytes(4);
             if (finalBytes != null)
             {
-                if (isPacked)
-                    finalBytes = BitConverter.GetBytes(0);
-                else
-                {
-                    finalBytesVal = BitConverter.ToInt32(finalBytes, 0);
+                finalBytesVal = BitConverter.ToInt32(finalBytes, 0);
 
-                    if (finalBytesVal > 100000)
-                    {
-                        // goto LABEL_55
-                    }
-                    k = finalBytesVal;
+                if (finalBytesVal > 100000)
+                {
+                    archive.RaiseError();
+                    return;
                 }
+                k = finalBytesVal;
             }
             // SmartArray::SetNElements(Ints, k, 1) -- truncate?
             Ints.RemoveRange(k - 1, Ints.Count - k);
 
             if (k == 0)
             {
-                // LABEL_53:
                 Sorted = true;
                 return;
             }
@@ -164,7 +144,6 @@ namespace ACE.Entity
             var prevBytesVal = BitConverter.ToInt32(prevBytes, 0);
             var before = prevBytesVal;
             var lBytesVal = 0;
-            var y = 0;
 
             while (true)
             {
@@ -173,53 +152,52 @@ namespace ACE.Entity
                 var endBytesVal = BitConverter.ToInt32(endBytes, 0);
                 if (endBytesVal != 0)
                 {
-                    if (isPacked)
-                        endBytes = prevBytes;
-                    else
-                        prevBytes = endBytes;
+                    prevBytes = endBytes;
 
                     if ((prevBytesVal & 0x40000000) != 0)
                         prevBytesVal |= -2147483648;
 
                     Ints[l++] = (int)prevBytesVal;
-                    // goto LABEL_52
+                    if (l >= finalBytesVal)
+                    {
+                        Sorted = true;
+                        return;
+                    }
                 }
-
-                prevBytesVal = -prevBytesVal;
-
-                archive.CheckAlignment(4);
-
-                var lBytes = archive.GetBytes(4);
-                lBytesVal = BitConverter.ToInt32(lBytes, 0);
-
-                if (lBytes != null)
+                else
                 {
-                    if (isPacked)
-                        lBytes = BitConverter.GetBytes(before);
-                    else
+                    prevBytesVal = -prevBytesVal;
+
+                    archive.CheckAlignment(4);
+
+                    var lBytes = archive.GetBytes(4);
+                    lBytesVal = BitConverter.ToInt32(lBytes, 0);
+
+                    if (lBytes != null)
                         before = lBytesVal;
-                }
 
-                if (prevBytesVal > 0)
-                    break;
+                    if (prevBytesVal > 0)
+                        break;
 
-                // LABEL_52:
-                if (l >= finalBytesVal)
-                {
-                    // goto LABEL_53
+                    if (l >= finalBytesVal)
+                    {
+                        Sorted = true;
+                        return;
+                    }
                 }
             }
 
+            var y = 0;
             while (l < finalBytesVal)
             {
                 Ints[l++] = lBytesVal;
                 lBytesVal++;    // increment into archive buffer
-                if (++y >= prevBytesVal)
+                if (++y >= prevBytesVal && l > finalBytesVal)
                 {
-                    // goto LABEL_52
+                    Sorted = true;
+                    return;
                 }
             }
-            // LABEL_55:
             archive.RaiseError();
         }
     }
