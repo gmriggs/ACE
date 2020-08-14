fcb516b5549b71bec642c68f21847bb5d7c4176d
gmriggs <gmriggs@gmail.com>
2020-08-14 03:10:38 -0400

    adding TreasureTableType enum

---
+++ Source/ACE.Server/Factories/Treasure/Struct/TreasureTableType.cs
@@ -0,0 +1,21 @@
+namespace ACE.Server.Factories.Treasure.Struct
+{
+    public enum TreasureTableType
+    {
+        Undef,
+        Coins,
+        Gem,
+        Jewelry,
+        ArtObject,
+        Weapon,
+        Armor,
+        Clothing,
+        Scroll,
+        Caster,
+        ManaStone,
+        FoodDrink,
+        HealKit,
+        Lockpick,
+        SpellComponent
+    }
+}
