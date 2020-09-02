4b8593439bd755c42ced1240a2d59eef57c0f579
gmriggs <gmriggs@gmail.com>
2020-09-02 10:01:23 -0400

    adding missing files

---
+++ Source/ACE.Database/Models/World/TreasureGemValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ACE.Database.Models.World
+{
+    public partial class TreasureGemValue
+    {
+        public uint Id { get; set; }
+        public int Class { get; set; }
+        public int Value { get; set; }
+    }
+}
